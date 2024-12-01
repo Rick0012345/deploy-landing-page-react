8b8885ca789e25ad496b84b63f0066b1a7ad9725
patrick <po44243@gmail.com.com>
2024-11-30 22:08:23 -0300

    vite

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        
+        <Body/>
+
+      
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+// import React from "react";
+import '../static/css/Navbar.css';
+
+function Navbar() {
+  const handleMenuClick = () => {
+    // Lógica para exibir/esconder o menu em telas menores
+    const nav = document.querySelector(".navbar ul");
+    nav.classList.toggle("show");
+  };
+
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="logo">
+          <h1>Projeto Sanba</h1>
+        </div>
+        <ul>
+          <li>
+            <a href="#">HOME</a>
+          </li>
+          <li>
+            <a href="#">SOBRE NOS</a>
+          </li>
+          <li>
+            <a href="/accounts/logout/">LOGOUT</a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CONTATOS
+            </a>
+          </li>
+        </ul>
+        <div className="menu-icon" onClick={handleMenuClick}>
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
